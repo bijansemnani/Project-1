1a6efd05db13e1751b2c12a25a96a0f2efea9a85
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-01 13:14:36 -0800

    ready to start

--- assets/javascript/spotify.js
+++ assets/javascript/spotify.js
@@ -1,15 +1,13 @@
 $(document).ready(function () {
   var queryUrl = "https://api.spotify.com/v1/search";
-  var query = "maroon5";
-  $.ajax({
-    type: "POST"
-  })
+  var query = "avicii";
+
   $.ajax({
         url: 'https://api.spotify.com/v1/search',
         headers:{"Authorization": "Bearer BQDTmUZ1UXGIND2hVrPnc7H_9oWUGCBQbJsI1RWk9e1JP6v3JsbMh7abN1iDvacqx1O5yIyXjn-iJiWV9fKCtEe8SW8CUEoI_tpQDEkLAnkbsmhT_DiFFwymYwfk4pDDlr7jiQLaIA"},
         data: {
             q: query,
-            type: 'album'
+            type: 'artist'
         },
         success: function (response) {
             console.log(response);
