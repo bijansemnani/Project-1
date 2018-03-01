481d03cd4826c93990cc07ff968a52a095d57421
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-01 11:27:53 -0800

    connecting the spotfiy api to the app

---
+++ assets/javascript/spotify.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  var queryUrl = "https://api.spotify.com/v1/search";
+  var query = "maroon5";
+  $.ajax({
+    type: "POST"
+  })
+  $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        headers:{"Authorization": "Bearer BQDTmUZ1UXGIND2hVrPnc7H_9oWUGCBQbJsI1RWk9e1JP6v3JsbMh7abN1iDvacqx1O5yIyXjn-iJiWV9fKCtEe8SW8CUEoI_tpQDEkLAnkbsmhT_DiFFwymYwfk4pDDlr7jiQLaIA"},
+        data: {
+            q: query,
+            type: 'album'
+        },
+        success: function (response) {
+            console.log(response);
+        }
+    });
+
+
+
+
+});
