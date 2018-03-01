eff538700465180af497e2c1852e711a739ec41d
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-01 13:28:12 -0800

    working on the spotify api

--- assets/javascript/spotify.js
+++ assets/javascript/spotify.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
   var queryUrl = "https://api.spotify.com/v1/search";
-  var query = "maroon5";
-  $.ajax({
-    type: "POST"
-  })
+  var query;
+
+function ajaxCall(query) {
   $.ajax({
         url: 'https://api.spotify.com/v1/search',
         headers:{"Authorization": "Bearer BQDTmUZ1UXGIND2hVrPnc7H_9oWUGCBQbJsI1RWk9e1JP6v3JsbMh7abN1iDvacqx1O5yIyXjn-iJiWV9fKCtEe8SW8CUEoI_tpQDEkLAnkbsmhT_DiFFwymYwfk4pDDlr7jiQLaIA"},
@@ -15,8 +14,11 @@ $(document).ready(function () {
             console.log(response);
         }
     });
-
-
+}
+  $("add-artst").on("click", function (event) {
+    event.preventDefault();
+    query = $("#artist-input").val();
+  });
 
 
 });
