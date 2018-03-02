3509d43848da36f405e8ee78f1959c0202e29df0
noble0423 <ryan.noble82@gmail.com>
2018-03-02 12:06:55 -0800

    for loop pulling correct date from querysearch

--- assets/javascript/bandsInTown.js
+++ assets/javascript/bandsInTown.js
@@ -1,11 +1,23 @@
 function searchBandsInTown(artist) {
     var artist = "beck";
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        //console.log(response);
+    
+        var results = response;
+        for (var i = 0; i < results.length; i++) {
+            //console.log(results[i]);
+            console.log(results[i].venue.name);
+            console.log(results[i].venue.city);
+            console.log(results[i].venue.region);
+            console.log(results[i].venue.country);
+            console.log(results[i].offers[0].url);
+            console.log("/////////////////////////////");
+        }
+
     });
 }
 
