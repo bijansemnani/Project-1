79cbe257d9fc77373b07bb14d1f54461054393d4
noble0423 <ryan.noble82@gmail.com>
2018-03-04 13:13:47 -0800

    added searchArtistInfo function and added an on-click event listener to bandsInTown.js., but it doesn't seem to be working properly.

--- assets/javascript/bandsInTown.js
+++ assets/javascript/bandsInTown.js
@@ -1,6 +1,5 @@
 function searchBandsInTown(artist) {
     //console.log(similarArtists);
-    var artist = similarArtists[0];
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     $.ajax({
         url: queryURL,
@@ -19,3 +18,31 @@ function searchBandsInTown(artist) {
 
     });
 }
+
+function searchArtistInfo(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        var artistName = $("<h1>").text(response.name);
+        console.log(artistName);
+        var artistURL = $("<a>").attr("href", response.url).append(artistName);
+        console.log(artistURL);
+        // var artistImage = ("<img>").attr("src", response.thumb_url);
+        // console.log(artistImage);
+        $("#artist-div").empty();
+        $("#artist-div").append(artistURL, artistImage);
+    });
+}
+
+$("#add-artist").on("click", function(event) {
+    event.preventDefault();
+    console.log("clicked");
+    var inputArtist = $("#artist-input").val().trim();
+    searchArtistInfo(inputArtist);
+});
+
+// $(document).on("ready")
+// searchArtistInfo();
