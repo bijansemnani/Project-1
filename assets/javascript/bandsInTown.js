2f1d9d1713a113c3562992fe60823d56f1dd0479
noble0423 <ryan.noble82@gmail.com>
2018-03-04 09:22:49 -0800

    added similar artist info to index.html

--- assets/javascript/bandsInTown.js
+++ assets/javascript/bandsInTown.js
@@ -1,16 +1,14 @@
 function searchBandsInTown(artist) {
-    console.log(similarArtists);
+    //console.log(similarArtists);
     var artist = similarArtists[0];
     var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        //console.log(response);
     
         var results = response;
         for (var i = 0; i < results.length; i++) {
-            //console.log(results[i]);
             console.log(results[i].venue.name);
             console.log(results[i].venue.city);
             console.log(results[i].venue.region);
