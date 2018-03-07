9e55dd1e028fe63401ae161b7c40f24e84d2be23
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-06 18:01:38 -0800

    added some more functions to bandsInTown to help clean and section the code so its easier to follow

--- assets/javascript/bandsInTown.js
+++ assets/javascript/bandsInTown.js
@@ -62,6 +62,44 @@ function listener(response, marker) {
   eventinfoWindow.open(map, marker);
 }
 
+function similarArtistDiv(response,artist) {
+  var datetime = (response.datetime);
+  var datetimeSplit = datetime.split("T");
+  var eventDate = datetimeSplit[0]; //EVENT DATE
+  var eventTime = datetimeSplit[1];
+
+  var eventInfoDiv = $("<div>");
+  eventInfoDiv.addClass("col s3 m6");
+
+  var eventInfoDiv2 = $("<div>");
+  eventInfoDiv2.addClass("card blue-grey darken-1");
+
+  var eventInfoDiv3 = $("<div>");
+  eventInfoDiv3.addClass("card-content white-text");
+
+  var eventInfoSpan =
+  $("<span class = 'card-title'>" + artist + "</span>"
+  + "<p>Venue: "    + response.venue.name    + "</p>"
+  + "<p>Location: " + response.venue.city    + ", "
+                    + response.venue.region  + ", "
+                    + response.venue.country + "</p>"
+  + "<p>Date: "     + eventDate + "</p>"
+  + "<p>Time: "     + eventTime + "</p>");
+
+  var ticketInfoDiv = $("<div>");
+  ticketInfoDiv.addClass("card-action");
+
+  var ticketInfoA = $("<a>").text("BUY TICKETS");
+  ticketInfoA.attr("target", "_blank");
+  ticketInfoA.attr("href", response.offers[0].url);
+  ticketInfoDiv.append(ticketInfoA);
+
+  $("#similarArtistEvents").append(eventInfoDiv);
+  eventInfoDiv.append(eventInfoDiv2);
+  eventInfoDiv2.append(eventInfoDiv3, ticketInfoDiv);
+  eventInfoDiv3.append(eventInfoSpan);
+}
+
 function searchEventsInTown(artist, isTrue) {
    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
    var icon = 'https://maps.google.com/mapfiles/kml/paddle/wht-stars-lv.png';
@@ -75,28 +113,10 @@ function searchEventsInTown(artist, isTrue) {
        for (var i = 0; i < response.length; i++){
          var venueLatitudeString = (response[i].venue.latitude);
          var venueLongitudeString = (response[i].venue.longitude);
-         var datetime = (response[i].datetime);
-         var datetimeSplit = datetime.split("T");
-         //--------------------------------Concert & Artist Information------------------------//
-         //BELOW ARE THE USEFUL VARIABLES
-         var venueName = (response[i].venue.name); //EVENT VENUE NAME
-         var venueCity = (response[i].venue.city); //EVENT CITY
-         var venueState = (response[i].venue.region); //EVENT STATE OR REGION
-         var venueCountry = (response[i].venue.country); //EVENT COUNTRY
-
-         var eventDate = datetimeSplit[0]; //EVENT DATE
-         var eventTime = datetimeSplit[1]; //EVENT TIME
-
-         var venueLat = Number(venueLatitudeString); //EVENT LATITUDE
-         var venueLong = Number(venueLongitudeString); //EVENT LONGITUDE
-
-         //Set the eventLocation as a google maps LatLong location
+         var venueLat = Number(venueLatitudeString);
+         var venueLong = Number(venueLongitudeString);
          var eventLocation = new google.maps.LatLng(venueLat,venueLong)
 
-         var eventTicket = (response[i].offers[0].url); //LINK TO PURCHASE TICKETS
-         var eventTicketStatus = (response[i].offers[0].status); //EVENT TICKET AVAILABILITY
-
-         //--------------------------------Event Marker Creation-------------------------------//
          //Check to see if the location is within the given radius
          if(bounds.contains(eventLocation) === true){
            //Create a marker for each event on the map save the index and artist name for later use
@@ -106,43 +126,13 @@ function searchEventsInTown(artist, isTrue) {
                icon: icon,
                index: i,
                artist: artist
-
            //Add a click listener to each marker
            }).addListener('click',function () {
              listener(response, this);
            });
-
             markers.push(eventMarker);
          }
-         //------------------------------------------------------------------------------------------------------
-         var eventInfoDiv = $("<div>");
-         eventInfoDiv.addClass("col s3 m6");
-
-         var eventInfoDiv2 = $("<div>");
-         eventInfoDiv2.addClass("card blue-grey darken-1");
-
-         var eventInfoDiv3 = $("<div>");
-         eventInfoDiv3.addClass("card-content white-text");
-
-         var eventInfoSpan = $("<span class = 'card-title'>" + artist + "</span>"
-         + "<p>Venue: " + venueName + "</p>"
-         + "<p>Location: " + venueCity + ", " + venueState + ", " + venueCountry + "</p>"
-         + "<p>Date: " + eventDate + "</p>"
-         + "<p>Time: " + eventTime + "</p>");
-
-         var ticketInfoDiv = $("<div>");
-         ticketInfoDiv.addClass("card-action");
-         var ticketInfoA = $("<a>").text("BUY TICKETS");
-         ticketInfoA.attr("target", "_blank");
-         ticketInfoA.attr("href", eventTicket);
-         ticketInfoDiv.append(ticketInfoA);
-         $("#similarArtistEvents").append(eventInfoDiv);
-         eventInfoDiv.append(eventInfoDiv2);
-         eventInfoDiv2.append(eventInfoDiv3, ticketInfoDiv);
-         eventInfoDiv3.append(eventInfoSpan);
-
-
-
+         similarArtistDiv(response[i],artist);
        }
    });
 }
